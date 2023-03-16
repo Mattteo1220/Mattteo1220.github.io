2a1595660e304351cb31bd543ee143f42f2d197d
matt Mcentire <Msmcentire1220@outlook.com>
2023-03-16 14:09:08 -0600

    Begin styling and adding js functionality to the website

---
+++ index.js
@@ -0,0 +1,16 @@
+let sqaStartYear = 2015;
+let seStartYear = 2021;
+
+let setExperienceYears = () => {
+    let sqaElement = document.getElementById("sqa").getElementsByTagName("h2")[0];
+    let sqaYearsOfExperience = new Date().getFullYear() - sqaStartYear;
+    sqaElement.innerHTML = `${sqaYearsOfExperience}+ Years`;
+    console.log("Set the experience for sqa");
+
+    let seElement = document.getElementById("se").getElementsByTagName("h2")[0];
+    let seYearsOfExperience = new Date().getFullYear() - seStartYear;
+    seElement.innerHTML = `${seYearsOfExperience}+ Years`;
+    console.log("Set the experience for se");
+}
+
+setExperienceYears();
